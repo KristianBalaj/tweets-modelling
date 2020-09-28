7465b8deb0fd8acb3c08ae8b99e431e6ab3a675c
Kristian Balaj <kiko.balaj@gmail.com>
2020-09-29 00:58:42 +0200

    feat(*): parsing zipped jsonl tweets files

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,17 @@
 
 module Main where
 
+import qualified Codec.Compression.GZip as GZip
+import Control.Monad
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as B
+import Data.List
+import qualified Data.Text.Lazy as L
+import Data.Text.Lazy.Encoding
 import Database.PostgreSQL.Simple
 import Models.Tweet
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath (combine)
 
 connectionString :: ByteString
 connectionString =
@@ -22,11 +29,19 @@ connectionString =
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  tweet <- fmap parseTweet $ B.readFile "test.json"
-  print tweet
+  tweetLines <- tweetsByLines
+  print $ map (parseTweet . encodeUtf8) tweetLines
 
-  -- conn <- connectPostgreSQL connectionString
-  -- res <- query_ conn "select * from users" :: IO [String]
-  -- mapM print res
-  return ()
+tweetsByLines :: IO [L.Text]
+tweetsByLines =
+  let tweetFiles = tweetsZippedFilePaths "data" >>= traverse B.readFile
+   in fmap join $ map (L.lines . decodeUtf8 . GZip.decompress) <$> tweetFiles
+
+tweetsZippedFilePaths :: FilePath -> IO [String]
+tweetsZippedFilePaths dirPath = do
+  exists <- doesDirectoryExist dirPath
+  if exists
+    then do
+      fileNames <- filter (isSuffixOf ".jsonl.gz") <$> getDirectoryContents dirPath
+      return $ map (combine dirPath) fileNames
+    else fail $ "No such directory exists (directory=\"" ++ dirPath ++ "\")!"
