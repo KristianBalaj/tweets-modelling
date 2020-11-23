d31ae08c027be496955bb6ccb769268ceef45dd0
Kristián Balaj <kiko.balaj@gmail.com>
2020-11-23 14:56:58 +0100

    feat(*): adding es mapping for tweet index

--- src/Elastic/ElasticTweet.hs
+++ src/Elastic/ElasticTweet.hs
@@ -10,28 +10,28 @@ import Models.Tweet
 
 newtype ElasticTweet = ElasticTweet Tweet
 
--- |  Similar to `object` function except this removed all the `Null` values.
--- Credits to: https://stackoverflow.com/questions/19665099/data-aeson-encoding-optional-keys#comment43757457_19666176
+-- | Similar to `object` function except this removes all the `Null` values from the result JSON.
+-- | Credits to: https://stackoverflow.com/questions/19665099/data-aeson-encoding-optional-keys#comment43757457_19666176
 objectNoNulls :: [Pair] -> Value
 objectNoNulls = object . filter (\(_, value) -> value /= Null)
 
 instance ToJSON ElasticTweet where
   toJSON (ElasticTweet tweet) =
     objectNoNulls
-      [ "id_str" .= tweetId tweet,
-        "tweet_text" .= tweetContent tweet,
-        "coordinates"
+      [ "id" .= tweetId tweet,
+        "tweetContent" .= tweetContent tweet,
+        "location"
           .= ( tweetLocation tweet
                  <&> ( \coords ->
                          object ["lat" .= latitude coords, "lon" .= longitude coords]
                      )
              ),
-        "retweet_count" .= retweetCount tweet,
-        "favorite_count" .= favoriteCount tweet,
-        "created_at" .= happenedAt tweet,
+        "retweetCount" .= retweetCount tweet,
+        "favoriteCount" .= favoriteCount tweet,
+        "createdAt" .= happenedAt tweet,
         "author" .= tweetAuthor tweet,
-        "place" .= tweetCountry tweet,
-        "parent_tweet_id" .= (tweetId <$> parentTweet tweet),
+        "country" .= tweetCountry tweet,
+        "parentTweetId" .= (tweetId <$> parentTweet tweet),
         "mentions" .= mentionedUsers tweet,
         "hashtags" .= tweetHashtags tweet
       ]
